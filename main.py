cf285c79864b28c729c93e084dce7b6ac857e424
hwkim <hwkim.vdrc@example.com>
2023-02-05 12:36:59 +0000

    pdb moved

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os, sys, platform
 import time
+import 
 
 from channelValuator import ChannelValuator
 
@@ -26,23 +27,23 @@ gridSize            = 0.5
 
 CV = ChannelValuator()
 
-print('1.set_protein')
+# print('1.set_protein')
 CV.set_protein(pdbFileName, includeHETATM)
-print('2.set_channels')
+# print('2.set_channels')
 CV.set_channels(channelFilePaths)
 
-print('3.inflate_protein')
+# print('3.inflate_protein')
 CV.inflate_protein(cutoffRatio, initialIncrement, step)
-print('4.set_grid')
+# print('4.set_grid')
 CV.set_grid(gridSize)
 
-print('5.set_ground_truth')
+# print('5.set_ground_truth')
 CV.set_ground_truth()
-print('6.verify_atom_overlapping_vertices')
+# print('6.verify_atom_overlapping_vertices')
 CV.verify_atom_overlapping_vertices()
-print('7.verify_channel_overlapping_vertices')
+# print('7.verify_channel_overlapping_vertices')
 CV.verify_channel_overlapping_vertices()
 
-print('8.write_result_in_PyMOL_script')
+# print('8.write_result_in_PyMOL_script')
 CV.write_result_in_PyMOL_script(pdbFileName.replace('.pdb', '_CV_result.py'))
-print('Done')
+# print('Done')
